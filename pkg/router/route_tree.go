89ce561604d99f8c2ac50824a59c6cc65bb1aaa9
Derek Rushing <20804720+drush211@users.noreply.github.com>
2025-12-06 20:32:31 -0600

    Add the ability to add a route tree route given a chi router. (#9)
    * Add the ability to add a route tree route given a chi router.
    
    * Update route_tree.go

---
+++ pkg/router/route_tree.go
@@ -0,0 +1,45 @@
+package router
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// RouteTree will produce a map of string -> []string for all method + route combos that aren't excluded because of
+// their prefix. This should be used like
+//
+//	r.Get("/routes", router.RouteTree(r, []string{
+//		"/assets",
+//		"/metrics",
+//		"/routes",
+//	 ...
+//	}))
+func RouteTree(router chi.Router, excludeRouteStartsWith []string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		routeInfo := make(map[string][]string)
+		walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+			route = strings.ReplaceAll(route, "/*/", "/")
+			for _, exclude := range excludeRouteStartsWith {
+				if strings.HasPrefix(route, exclude) {
+					return nil
+				}
+			}
+
+			if routeInfo[method] == nil {
+				routeInfo[method] = make([]string, 0)
+			}
+			routeInfo[method] = append(routeInfo[method], route)
+			return nil
+		}
+
+		if err := chi.Walk(router, walkFunc); err != nil {
+			fmt.Printf("Logging err: %s\n", err.Error())
+		}
+		routeInfoBytes, _ := json.Marshal(routeInfo)
+		_, _ = w.Write(routeInfoBytes)
+	}
+}
