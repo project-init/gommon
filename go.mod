4d83e39f38a3e6c6e8aad1a814b4dd0785e13bf5
Derek Rushing <20804720+drush211@users.noreply.github.com>
2025-12-11 06:58:14 -0600

    INIT-235: Add connection setup for postgres.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/project-init/gommon
 go 1.25.4
 
 require (
+	github.com/aws/aws-sdk-go-v2/feature/rds/auth v1.6.16
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.6
@@ -16,6 +17,8 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.41.0 // indirect
+	github.com/aws/smithy-go v1.24.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -23,6 +26,9 @@ require (
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/grafana/regexp v0.0.0-20240518133315-a468a5bfb3bc // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
@@ -34,7 +40,9 @@ require (
 	go.opentelemetry.io/otel/sdk v1.38.0 // indirect
 	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	golang.org/x/crypto v0.43.0 // indirect
 	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
---
+++ pkg/postgres/connection.go
@@ -0,0 +1,113 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/rds/auth"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	gerror "github.com/project-init/gommon/pkg/errors"
+)
+
+type ConnectionConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Database string
+	Password string
+
+	MaxConnections  *int
+	MinConnections  *int
+	MaxConnLifetime *string
+	MaxConnIdleTime *string
+}
+type IAMConfig struct {
+	Credentials aws.CredentialsProvider
+	Region      string
+}
+
+// NewConnection Creates a connection pool to the database defined in the ConnectionConfig. If an IAMConfig is given,
+// then it is assumed to be using rds connect and the connection lifetime values will be overwritten.
+func NewConnection(ctx context.Context, connectionConfig *ConnectionConfig, iamConfig *IAMConfig) (*pgxpool.Pool, error) {
+	connStr, err := connectionString(ctx, connectionConfig, iamConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	pgxCfg, err := pgxpool.ParseConfig(connStr)
+	if err != nil {
+		return nil, fmt.Errorf("parse postgres conn string: %w", err)
+	}
+
+	if iamConfig != nil {
+		pgxCfg.BeforeConnect = func(ctx context.Context, config *pgx.ConnConfig) error {
+			authToken, err := buildAuthToken(ctx, *connectionConfig, *iamConfig)
+			if err != nil {
+				return err
+			}
+			config.Password = authToken
+			return nil
+		}
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgxCfg)
+	if err != nil {
+		return nil, fmt.Errorf("%w: could not connect to postgres", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("%w: could not ping postgres", err)
+	}
+
+	return pool, nil
+}
+
+func connectionString(ctx context.Context, connectionConfig *ConnectionConfig, iamConfig *IAMConfig) (string, error) {
+	if connectionConfig == nil {
+		return "", fmt.Errorf("%w: connection config is required", gerror.ErrPreconditionFailed)
+	}
+
+	maxConnections := 5
+	minConnections := 1
+	// Setting Max connection lifetime and idle time to 14m as the token we get from iam is good for only 15m
+	maxConnLifetime := "14m"
+	maxConnIdleTime := "14m"
+
+	password := connectionConfig.Password
+	sslMode := "disable"
+	if iamConfig != nil {
+		authToken, err := buildAuthToken(ctx, *connectionConfig, *iamConfig)
+		if err != nil {
+			return "", err
+		}
+		password = authToken
+		sslMode = "require"
+	}
+
+	if connectionConfig.MaxConnections != nil && *connectionConfig.MaxConnections > 0 {
+		maxConnections = *connectionConfig.MaxConnections
+	}
+	if connectionConfig.MinConnections != nil && *connectionConfig.MinConnections > 0 {
+		minConnections = *connectionConfig.MinConnections
+	}
+	// If iamConfig is given, then do not override the default for connection lifetime and idle time.
+	if connectionConfig.MaxConnLifetime != nil && iamConfig == nil {
+		maxConnLifetime = *connectionConfig.MaxConnLifetime
+	}
+	if connectionConfig.MaxConnIdleTime != nil && iamConfig == nil {
+		maxConnIdleTime = *connectionConfig.MaxConnIdleTime
+	}
+
+	hostInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", connectionConfig.Host, connectionConfig.Port, connectionConfig.User, password, connectionConfig.Database, sslMode)
+	connectionInfo := fmt.Sprintf("pool_max_conns=%d pool_min_conns=%d pool_max_conn_lifetime=%s pool_max_conn_idle_time=%s", maxConnections, minConnections, maxConnLifetime, maxConnIdleTime)
+	return fmt.Sprintf("%s %s", hostInfo, connectionInfo), nil
+}
+
+func buildAuthToken(ctx context.Context, connectionConfig ConnectionConfig, iamConfig IAMConfig) (string, error) {
+	return auth.BuildAuthToken(ctx, fmt.Sprintf("%s:%s", connectionConfig.Host, connectionConfig.Port), iamConfig.Region, connectionConfig.User, iamConfig.Credentials)
+}
