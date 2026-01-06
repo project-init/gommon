fedf6d31b262919fc14bae2ee4a71231c226b7fe
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-01-06 07:17:42 -0600

    Update cognito.go

--- pkg/auth/cognito.go
+++ pkg/auth/cognito.go
@@ -7,6 +7,33 @@ package auth
 //		// Cognito
 //		CognitoConfig auth.CognitoConfig `env:"COGNITO" yaml:"cognito" safe:"true"`
 //	}
+//
+//	var (
+//		configuration *Config = &Config{}
+//		once          sync.Once
+//	)
+//
+//	func GetConfig() *Config {
+//		once.Do(func() {
+//			opts := []cfg.Option{}
+//
+//			if configFiles, ok := os.LookupEnv(EnvVarConfigFiles); ok && len(configFiles) > 0 {
+//				opts = append(opts, cfg.NewFileOption(configFiles))
+//			}
+//
+//			if smSecrets, ok := os.LookupEnv(EnvVarConfigAWSSecretsManagerSecrets); ok && len(smSecrets) > 0 {
+//				opts = append(opts, cfg.NewAWSSecretsManagerOption(secretsmanager.NewFromConfig(aws.GetConfig()), strings.Split(smSecrets, ",")...))
+//			}
+//
+//			opts = append(opts, cfg.NewEnvOption())
+//
+//			if err := cfg.LoadConfigs(configuration, opts...); err != nil {
+//				sre.LogFatal(err)
+//			}
+//		})
+//
+//		return configuration
+//	}
 type CognitoConfig struct {
 	UseLocal   bool   `env:"COGNITO_USE_LOCAL" yaml:"cognitoUseLocal" envDefault:"false" safe:"true"`
 	Endpoint   string `env:"COGNITO_ENDPOINT" yaml:"cognitoEndpoint" safe:"true"`
