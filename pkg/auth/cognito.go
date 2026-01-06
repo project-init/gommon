4742f58f7d8e1c4e770886fb278a38941896a09e
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-01-06 06:57:51 -0600

    Update cognito.go

--- pkg/auth/cognito.go
+++ pkg/auth/cognito.go
@@ -1,7 +1,15 @@
 package auth
 
+// CognitoConfig Struct meant to wrap configuration values to work alongside the https://github.com/project-init/terraform-aws-cognito
+// terraform module. Usage would look similar to
+//
+//	type Config struct {
+//		// Cognito
+//		CognitoConfig auth.CognitoConfig `env:"COGNITO" yaml:"cognito" safe:"true"`
+//	}
 type CognitoConfig struct {
-	CognitoEndpoint   string `env:"COGNITO_ENDPOINT" yaml:"cognitoEndpoint" safe:"true"`
-	CognitoClientId   string `env:"COGNITO_CLIENT_ID" json:"cognitoClientId" yaml:"cognitoClientId"`
-	CognitoUserPoolId string `env:"COGNITO_USER_POOL_ID" json:"cognitoUserPoolId" yaml:"cognitoUserPoolId"`
+	UseLocal   bool   `env:"COGNITO_USE_LOCAL" yaml:"cognitoUseLocal" envDefault:"false" safe:"true"`
+	Endpoint   string `env:"COGNITO_ENDPOINT" yaml:"cognitoEndpoint" safe:"true"`
+	ClientId   string `env:"COGNITO_CLIENT_ID" json:"cognitoClientId" yaml:"cognitoClientId"`
+	UserPoolId string `env:"COGNITO_USER_POOL_ID" json:"cognitoUserPoolId" yaml:"cognitoUserPoolId"`
 }
