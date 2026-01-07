be698ffb3413a771e0f9090d9dd2a2e02c1738d1
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-01-07 09:44:28 -0600

    Update cognito.go

--- pkg/cfg/configs/cognito.go
+++ pkg/cfg/configs/cognito.go
@@ -35,8 +35,10 @@ package configs
 //		return configuration
 //	}
 type Cognito struct {
-	UseLocal   bool   `env:"COGNITO_USE_LOCAL" yaml:"cognitoUseLocal" envDefault:"false" safe:"true"`
-	Endpoint   string `env:"COGNITO_ENDPOINT" yaml:"cognitoEndpoint" safe:"true"`
-	ClientId   string `env:"COGNITO_CLIENT_ID" json:"cognitoClientId" yaml:"cognitoClientId"`
-	UserPoolId string `env:"COGNITO_USER_POOL_ID" json:"cognitoUserPoolId" yaml:"cognitoUserPoolId"`
+	UseLocal    bool   `env:"COGNITO_USE_LOCAL" yaml:"cognitoUseLocal" envDefault:"false" safe:"true"`
+	Endpoint    string `env:"COGNITO_ENDPOINT" yaml:"cognitoEndpoint" safe:"true"`
+	ClientId    string `env:"COGNITO_CLIENT_ID" json:"cognitoClientId" yaml:"cognitoClientId"`
+	UserPoolId  string `env:"COGNITO_USER_POOL_ID" json:"cognitoUserPoolId" yaml:"cognitoUserPoolId"`
+	IssuerUrl   string `env:"COGNITO_ISSUER_URL" yaml:"cognitoIssuerUrl"`
+	RedirectUrl string `env:"COGNITO_REDIRECT_URL" yaml:"cognitoRedirectUrl"`
 }
