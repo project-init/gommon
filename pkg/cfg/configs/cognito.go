4ed14dc59f089089c2f0b231e2fc8bbb5b51be33
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-01-07 09:46:01 -0600

    Update cognito.go

--- pkg/cfg/configs/cognito.go
+++ pkg/cfg/configs/cognito.go
@@ -35,10 +35,15 @@ package configs
 //		return configuration
 //	}
 type Cognito struct {
-	UseLocal    bool   `env:"COGNITO_USE_LOCAL" yaml:"cognitoUseLocal" envDefault:"false" safe:"true"`
-	Endpoint    string `env:"COGNITO_ENDPOINT" yaml:"cognitoEndpoint" safe:"true"`
-	ClientId    string `env:"COGNITO_CLIENT_ID" json:"cognitoClientId" yaml:"cognitoClientId"`
-	UserPoolId  string `env:"COGNITO_USER_POOL_ID" json:"cognitoUserPoolId" yaml:"cognitoUserPoolId"`
-	IssuerUrl   string `env:"COGNITO_ISSUER_URL" yaml:"cognitoIssuerUrl"`
+	UseLocal   bool   `env:"COGNITO_USE_LOCAL" yaml:"cognitoUseLocal" envDefault:"false" safe:"true"`
+	Endpoint   string `env:"COGNITO_ENDPOINT" yaml:"cognitoEndpoint" safe:"true"`
+	ClientId   string `env:"COGNITO_CLIENT_ID" json:"cognitoClientId" yaml:"cognitoClientId"`
+	UserPoolId string `env:"COGNITO_USER_POOL_ID" json:"cognitoUserPoolId" yaml:"cognitoUserPoolId"`
+	IssuerUrl  string `env:"COGNITO_ISSUER_URL" yaml:"cognitoIssuerUrl"`
+
+	// This will need to be set by the user as it will be dependent on your setup. For example, you might have multiple
+	// callback urls that are valid, but if you are running against a live, prelive, or local environment the redirect
+	// will be different. Therefore, this is not set by the terraform module itself, but instead should be added by the
+	// user.
 	RedirectUrl string `env:"COGNITO_REDIRECT_URL" yaml:"cognitoRedirectUrl"`
 }
